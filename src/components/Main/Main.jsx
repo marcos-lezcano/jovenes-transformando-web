d85ccbac353a94efaac139de76ce2a3770a38e58
lezcanozarza <lezcanozarza@gmail.com>
2021-09-02 10:38:30 -0300

    main update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,33 +1,50 @@
-import React, {useState} from 'react'
-import Style from './Main.module.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Style from "./Main.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-//Secciones
-import Description from './Description/Description.jsx'
-import News from './News/News.jsx'
+//Componentes de Secciones
+import Description from "./Description/Description.jsx";
+import NewsContainer from "./News/NewsContainer.jsx";
+import ReferentsContainer from "./Referents/ReferentsContainer.jsx";
+import ProjectsContainer from "./projects/ProjectsContainer.jsx";
+import JoinUs from './JoinUs/JoinUs.jsx'
 
 const Main = () => {
-
-  const [data, setData] = useState({
-    image: 'https://res.cloudinary.com/jovenes-transformando/image/upload/v1629096992/P%C3%A1gina%20web/portada2_ysjffg.jpg'
-  })
-
   return (
-    <div>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       {/* Imagen de portada */}
       <img
-        src={data.image}
-        alt='coverimage'
-        className={Style.CoverImage}/>
+        src={
+          "https://res.cloudinary.com/jovenes-transformando/image/upload/v1629096992/P%C3%A1gina%20web/portada2_ysjffg.jpg"
+        }
+        alt="coverimage"
+        className={Style.CoverImage}
+      />
 
-      {/* Componente con el mensaje de bienvenida */}
-      <Description/>
+      {/* Mensaje de bienvenida */}
 
-    {/* Componente con las ultimas noticias */}
-      <News/>
+      <Description />
+
+      {/* Referentes*/}
+
+      <ReferentsContainer />
+
+      {/* Ultimas Noticias */}
+
+      <NewsContainer />
+
+      {/* Proyectos */}
+
+      <ProjectsContainer />
+
+      {/* Sumate a Militar */}
+
+      <JoinUs/>
 
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
